4312aefb8beca820fccdcf3ce9de5aa27bd9ac7d
juanferdeleon <dele17822@uvg.edu.gt>
2021-09-23 11:35:12 -0600

    Remove Java Project

--- src/main/java/Mensajero.java
+++
@@ -1,7 +0,0 @@
-public class Mensajero {
-
-    public Mensajero(){
-
-    }
-
-}
--- src/main/java/PrincipalCliente.java
+++
@@ -1,8 +0,0 @@
-public class PrincipalCliente {
-    public static void main(String args[]){
-
-        ManejadorJugador.getInstance().elegirNombre("DavisGT");
-
-        ManejadorJugador.getInstance().unirseAJuego();
-    }
-}
