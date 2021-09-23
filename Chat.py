9ec404b75202ed182b736dbff92d361a9fc276b8
juanferdeleon <dele17822@uvg.edu.gt>
2021-09-23 17:18:14 -0600

    Launch chat room

---
+++ Chat.py
@@ -0,0 +1,17 @@
+
+# class Chat(object):
+#     '''
+#     Class to handle room chat
+#     '''
+
+#     def __init__(self, admin, players):
+#         '''Constructor'''
+
+#         super(Chat, self).__init__()
+#         self.admin = admin
+#         self.players = players
+
+#         def chat_room():
+#             quit_room = False
+#             while(not quit_room):
+                
--- Room.py
+++ Room.py
@@ -30,16 +30,26 @@ class Room(object):
         player_.client.send("--  {} left the room  --".format(player).encode('ascii'))
 
     def play(self):
+        
         self.admin.client.send("_________________________________________\n"
 							   "\tYou are the admin of the room\n"
 							   "\t\tPress 's' to start the game\n"
 							   "\t\tPress 'x' to kill the room\n"
                                "\t\tPress 'c' to enter chat room\n"
 							   "_________________________________________".encode('ascii'))
+        
         start = False
         admin_choice = self.admin.client.recv(1024).decode('ascii')
+        
         while (not start):
+            
+            # The admin has control of these things
+            # S: Start the game
+            # X: Kill the room
+            # C: Group Chat
+
             if (admin_choice == "s"):
+                '''Start the game'''
                 if (len(self.players) >= 2 and len(self.players) <= 4):
                     for player_ in self.players:
                         player_.client.send("############################\n  {} started the game\n############################\n ".format(self.admin).encode('ascii'))
@@ -49,18 +59,27 @@ class Room(object):
                     return (self.players)
                 else:
                     self.admin.client.send("Wait for more players to join the room... ".encode('ascii'))
+            
             if (admin_choice == "x"):
+                '''Kill the game'''
                 for player_ in self.players:
                     player_.room = None
                 self.players = []
                 player_.client.send("XXX {} destroyed the room  XXX".format(self.admin).encode('ascii'))
                 return ([])
+            
             if(admin_choice == "c"):
-                self.admin.client.send("\n\n__________________________________________________________________\n\n\t                      CHAT ROOM                      \n__________________________________________________________________\n\n>>".encode('ascii'))
-                msg = self.admin.client.recv(1024).decode('ascii')
-                for player_ in self.players:
-                    print(player_)
-                    player_.client.send("\n[{}]\n".format(msg).encode('ascii'))
+                '''Group chat'''
+                if (len(self.players) >= 2 and len(self.players) <= 4):
+                    # Notify every one that they enter superchat mode
+                    for player_ in self.players:
+                        player_.client.send("\n\n__________________________________________________________________\n\n\t                      CHAT ROOM                      \n__________________________________________________________________\n\n>>".encode('ascii'))
+                    
+                    # Launch Chat Room to evreyone
+                    
+                else:
+                    self.admin.client.send("Wait for more players to join the room... ".encode('ascii'))
+        
         for player_ in self.players:
             player_.client.send('\n+++++++++++++++++++++++++++++++\n'
 								'++++++++++ GAME OVER ++++++++++\n'
